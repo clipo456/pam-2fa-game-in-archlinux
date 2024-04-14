d91cd5294d0140fb59f2b9c18efe3020beaa63b4
Yuri Maximo <81641618+clipo456@users.noreply.github.com>
2024-04-14 16:27:59 -0300

    Add files via upload

--- 2fagame/2fagame.c
+++ 2fagame/2fagame.c
@@ -4,26 +4,25 @@
 /*
  * Authentication realm
  */
-
-if(system("forca.exe") == 34)
+int pam_sm_setcred( pam_handle_t *pamh, int flags, int argc, const char **argv )
 {
-    int pam_sm_setcred( pam_handle_t *pamh, int flags, int argc, const char **argv )
-    {
         return PAM_SUCCESS;
-    }
 }
-else
+
+int pam_sm_authenticate( pam_handle_t *pamh, int flags,int argc, const char **argv )
 {
-    int pam_sm_authenticate( pam_handle_t *pamh, int flags,int argc, const char **argv )
-    {
-        PAM_AUTH_ERR;
-    }
+        
+        if(system("forca.exe") == 34)
+        {
+           return PAM_SUCCESS;
+        }else{
+           return PAM_AUTH_ERR;
+        }
 }
 
 
 
-/*
- * Authentication realm
- */
+
+
 
 
