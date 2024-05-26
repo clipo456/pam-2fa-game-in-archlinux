99191c56a2573b26582f23e133ae4509081a2a9c
Yuri Maximo <81641618+clipo456@users.noreply.github.com>
2024-05-26 15:22:04 -0300

    Update 2fagame.c

--- 2fagame/2fagame.c
+++ 2fagame/2fagame.c
@@ -7,18 +7,18 @@
  */
 int pam_sm_setcred( pam_handle_t *pamh, int flags, int argc, const char **argv )
 {
-        return PAM_SUCCESS;
-}
-
-int pam_sm_authenticate( pam_handle_t *pamh, int flags,int argc, const char **argv )
-{
-        
         if(system("/root/pam-2fa-game-in-archlinux/2fagame/forca") == 34)
         {
            return PAM_SUCCESS;
         }else{
            return PAM_AUTH_ERR;
         }
+}
+
+int pam_sm_authenticate( pam_handle_t *pamh, int flags,int argc, const char **argv )
+{
+        
+        return PAM_SUCCESS;
 }
 
 
