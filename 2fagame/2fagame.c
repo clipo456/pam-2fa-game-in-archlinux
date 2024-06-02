a8b363d8689d3a6e61697444a5e9e1dcadfa1dc3
Yuri Maximo <81641618+clipo456@users.noreply.github.com>
2024-06-02 19:08:41 -0300

    Update 2fagame.c

--- 2fagame/2fagame.c
+++ 2fagame/2fagame.c
@@ -17,7 +17,7 @@ int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, const char **ar
     const void *user;
     ret = pam_get_item(pamh, PAM_USER, &user);
     if (ret != PAM_SUCCESS || user == NULL) {
-        pam_syslog(pamh, SIG_ERR, "Falha ao recuperar o nome de usuário ou autenticação primária falhou");
+        pam_syslog(pamh, LOG_ERR, "Falha ao recuperar o nome de usuário ou autenticação primária falhou");
         return PAM_AUTH_ERR;
     }
 
@@ -25,7 +25,7 @@ int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, const char **ar
     pid = fork();
     if (pid == -1) {
         // Falha ao criar um novo processo
-        pam_syslog(pamh, SIG_ERR, "Falha ao criar um novo processo");
+        pam_syslog(pamh, LOG_ERR, "Falha ao criar um novo processo");
         return PAM_AUTH_ERR;
     } else if (pid == 0) {
         // Processo filho
@@ -35,13 +35,22 @@ int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, const char **ar
     } else {
         // Processo pai
         if (waitpid(pid, &status, 0) == -1) {
-            pam_syslog(pamh, SIG_ERR, "Falha ao esperar o processo filho");
+            pam_syslog(pamh, LOG_ERR, "Falha ao esperar o processo filho");
             return PAM_AUTH_ERR;
         }
 
-        if (WIFEXITED(status) && WEXITSTATUS(status) == 34) {
-            return PAM_SUCCESS;
+        // Verifique o status de saída do processo filho
+        if (WIFEXITED(status)) {
+            int exit_status = WEXITSTATUS(status);
+            if (exit_status == 34) {
+                pam_syslog(pamh, LOG_INFO, "Autenticação adicional bem-sucedida com código de saída: %d", exit_status);
+                return PAM_SUCCESS;
+            } else {
+                pam_syslog(pamh, LOG_ERR, "Autenticação adicional falhou com código de saída: %d", exit_status);
+                return PAM_AUTH_ERR;
+            }
         } else {
+            pam_syslog(pamh, LOG_ERR, "Processo filho terminou de forma anormal");
             return PAM_AUTH_ERR;
         }
     }
